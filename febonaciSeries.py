1cc5961ab32c620de34dd1238f073333799312fc
vinaya <vinay.vinaykumar@gmail.com>
2022-01-17 10:41:40 -0500

    Changed

--- febonaciSeries.py
+++ febonaciSeries.py
@@ -13,11 +13,16 @@ def feb_ser_new(n):
         feb_list[n]=1
         return feb_list[n]
     else:
-        feb_list[n]= feb_ser_new(n-1)+feb_ser_new(n-2)
+        feb_list[n]= feb_ser_new(n-2)+feb_ser_new(n-1)
         return feb_list[n]
 
 
 
-print(feb_ser_new(50))
+print(feb_ser_new(75))
 print(feb_list)
 #print(feb_ser_new(25))
+
+#print(febi_ser(45))
+
+
+
