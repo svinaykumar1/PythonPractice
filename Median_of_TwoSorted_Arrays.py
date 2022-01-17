2a7f550ca7c05a4d168bd081539bf0765f28b498
vinaya <vinay.vinaykumar@gmail.com>
2022-01-17 10:41:51 -0500

    Changed

--- Median_of_TwoSorted_Arrays.py
+++ Median_of_TwoSorted_Arrays.py
@@ -1,17 +1,12 @@
 l1=[2, 3, 5, 6, 10, 12,28]
-l2=[1, 5, 11, 15, 18, 22, 72]
+l2=[1, 5, 11, 15, 18, 22, 45,58,72]
 
 l3=[]
 i=0
 j=0
 
 while (i<len(l1) and j<len(l2)):
-    if (i == (len(l1) - 1)):
-        l3.extend(l2[j:])
-        break
-    if (j == (len(l2) - 1)):
-        l3.extend(l1[i:])
-        break
+
     if (l1[i] < l2[j]):
         l3.append(l1[i])
         i = i + 1
@@ -19,10 +14,18 @@ while (i<len(l1) and j<len(l2)):
         l3.append(l2[j])
         j = j + 1
 
+    if (i == (len(l1) - 1)):
+        l3.extend(l2[j:])
+        break
+    if (j == (len(l2) - 1)):
+        l3.extend(l1[i:])
+        break
+
 print(l3)
 
 if(len(l3)%2==0):
-    median=(l3[len(l3)//2]+l3[len(l3)//2+1])/2
+    median=(l3[len(l3)//2-1]+l3[len(l3)//2])/2
 else:
     median=l3[len(l3)//2]
 print(median)
+
